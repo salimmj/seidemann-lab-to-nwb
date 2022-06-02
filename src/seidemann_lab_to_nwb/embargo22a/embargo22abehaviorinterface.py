e2071329a25bb1030ce762f34717148732c323d2
h-mayorquin <h.mayorquin@gmail.com>
2022-06-01 18:56:33 -0700

    added events

--- src/seidemann_lab_to_nwb/embargo22a/embargo22abehaviorinterface.py
+++ src/seidemann_lab_to_nwb/embargo22a/embargo22abehaviorinterface.py
@@ -3,12 +3,13 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
-from pymatreader import read_mat
+import scipy.io as sio
 
 from pynwb import NWBFile
 from nwb_conversion_tools.basedatainterface import BaseDataInterface
 from nwb_conversion_tools.utils.types import FolderPathType
-
+from ndx_events import LabeledEvents
+from nwb_conversion_tools.tools.nwb_helpers import get_module
 
 class Embargo22ABehaviorInterface(BaseDataInterface):
     """My behavior interface docstring"""
@@ -26,10 +27,8 @@ class Embargo22ABehaviorInterface(BaseDataInterface):
 
         session_path = Path(self.source_data["session_path"])
         file_path = session_path / "M22D20210127R0TS.mat"
-
-        data_simple = read_mat(str(file_path))
+        data_simple = sio.loadmat(str(file_path), simplify_cells=True)
         trial_structure = data_simple["TS"]
-
         # Mappings
         header = trial_structure["Header"]
         definitions = header["DEF"]
@@ -54,18 +53,17 @@ class Embargo22ABehaviorInterface(BaseDataInterface):
             "FPColor",
         ]
         df_trial_data = (
-            df_trial_data.replace(-1, np.nan)  # Replace -1 by NaN
+            df_trial_data.drop(columns=columns_with_structure)  # Drop columns with complex structure
+            .replace(-1, np.nan)  # Replace -1 by NaN
             .dropna(axis=1, how="all")  # Drop columns with only NaN
             .sort_values(by="TrialNum")  # Sort by the trial number
             .drop(columns=["TrialNum"])  # Drop trial number
-            .drop(columns=columns_with_structure)  # Drop columns with complex structure
         )
 
         # Remove columns with only one value
         all_columns = set(df_trial_data.columns.values)
         single_value_columns = [c for c in all_columns if len(df_trial_data[c].unique()) == 1]
-        columns_with_muliple_values = all_columns.difference(single_value_columns)
-        df_trial_data = df_trial_data[columns_with_muliple_values]
+        df_trial_data = df_trial_data.drop(columns=single_value_columns)
 
         # Add information to trial data
         df_trial_data["Outcome"] = [number_to_outcome_map[outcome].lower() for outcome in df_trial_data["Outcome"]]
@@ -94,11 +92,48 @@ class Embargo22ABehaviorInterface(BaseDataInterface):
 
         # Re-name
         df_trial_data.rename(
-            columns={"TimeTrialStart": "start_time", "TimeTrialEnd": "stop_time"},
-            inplace=True,
+            columns={"TimeTrialStart": "start_time", "TimeTrialEnd": "stop_time"}, inplace=True,
         )
-        for column in all_columns:
+
+        columns_to_add = [column for column in all_columns if column not in ["TimeTrialStart", "TimeTrialEnd"]]
+
+
+        for column in columns_to_add:
             nwbfile.add_trial_column(name=column, description=column)  # To-do add column description
         # Add the trials table
         rows_as_dicts = df_trial_data.T.to_dict().values()
         [nwbfile.add_trial(**row_dict) for row_dict in rows_as_dicts]
+
+        # Add Events
+        # For this conversion the types of events are the following
+        # ["type_eye_state", "type_protocol_state", "type_new_trial", "type_oi_state", "type_reward_state"]
+
+        behavior_module = get_module(nwbfile, "behavior")
+
+        number_to_defintion_map = {number: outcome for outcome, number in definitions["EVENT"].items()}
+
+        file_path_events = session_path / "events.csv"
+        df_events = pd.read_csv(file_path_events)
+        df_events["event_type"] = [number_to_defintion_map[number].lower() for number in df_events["Type"]]
+        df_events["event"] = [number_to_defintion_map[number].lower() for number in df_events["EventID"]]
+        df_events.drop(columns=["EventID", "Type", "TrialNum"], inplace=True)
+        df_events.rename(columns={"Timestamp": "timestamps"}, inplace=True)
+
+        event_type_array = df_events.event_type.unique()
+        for event_type in event_type_array:
+            df_event_type = df_events.query(f"event_type=='{event_type}'")
+            timestamps = df_event_type.timestamps.to_numpy()
+            labels = df_event_type.event.unique()
+            labels.sort()
+            label_to_postion = {label:position for position, label in enumerate(labels)}
+            data = [label_to_postion[label] for label in df_event_type.event]
+
+        
+            events = LabeledEvents(
+                name=event_type,
+                description=event_type,  # Look for descriptions
+                timestamps=timestamps,
+                data=data,
+                labels=labels,
+            )
+            behavior_module.add(events)
